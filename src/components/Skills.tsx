e53577345c94add6a9caa3274f660209c34569fc
Kevin Vu <kevin.vu@crometrics.com>
2023-03-19 20:56:03 -0500

    update skills and mobile

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -23,73 +23,73 @@ const Skills = () => {
       title: 'Javascript',
       img: javascript,
       level: 'advanced',
-      desc: 'programming language',
+      desc: 'Javascript is the most proficient language that I know. I learned javascript around the end of highschool when I started to learn how to make websites!',
     },
     {
       title: 'Python',
       img: python,
       level: 'intermediate',
-      desc: 'programming language'
+      desc: 'Python is the second language I picked up. I like python for its simplicity and readiablity. Python is my goto for non web applications.'
     },
     {
       title: 'HTML',
       img: html,
       level: 'advanced',
-      desc: 'markup language',
+      desc: 'Along with Javascript and CSS, I picked up HTML when I started making websites in highschool.',
     },
     {
       title: 'CSS',
       img: css,
       level: 'advanced',
-      desc: 'programming language'
+      desc: 'Along with Javascript and HTML, I picked up CSS when I started making websites in highschool.',
     },
     {
       title: 'Typescript',
       img: typescript,
       level: 'intermediate',
-      desc: 'syntactical superset of JavaScript'
+      desc: 'Adding Typescript to my work has improved my code styling and best practices. Helps me understand my code and the safety measures are a good plus!'
     },
     {
       title: 'React JS',
       img: reactjs,
       level: 'intermediate',
-      desc: 'front-end framework'
+      desc: 'This front-end framework is the I always come back to. The documentation is superb and is my go-to framework!'
     },
     {
-      title: 'Svelte',
+      title: 'Svelte / SvelteKit',
       img: svelte,
       level: 'intermediate',
-      desc: 'front-end framework'
+      desc: 'A front-end framework that focus on simplicity kind of like Python! I love how simple and lightweight it is. The plan is to utilize more Svelte/SvelteKit in future projects.'
     },
     {
       title: 'Optimizely',
       img: optimizely,
       level: 'intermediate',
-      desc: 'a/b testing platform'
+      desc: 'My go to  A/B testing platform and CMS platform!'
     },
     {
       title: 'Photoshop',
       img: photoshop,
-      level: 'intermediate',
-      desc: 'graphics editor'
+      level: 'advanced',
+      desc: 'The jack of all trades for editing my assets! I usually edit assets here, and draw art in Clip Studio Paint.'
     },
     {
       title: 'Figma',
       img: figma,
-      level: 'advanced',
-      desc: 'design software'
+      level: 'intermediate',
+      desc: `My go to design software. I know how to use Figma well, but that doesn't mean my designs are good. 😔`
     },
     {
       title: 'Blender',
       img: blender,
       level: 'casual',
-      desc: '3d modeling software'
+      desc: `My go to 3d modeling software. Just started exploring the 3D and the potential is there!`
     },
     {
       title: 'Kermit',
       img: kermit,
       level: 'casual',
-      desc: 'I can do voice impressions of kermit'
+      desc: `I can do voice impressions of kermit. It's up to you to judge if it's good enough. 😂`
     },
   ]
   const [ activeIndex, setActiveIndex ] = useState(0);
@@ -112,7 +112,7 @@ const Skills = () => {
           <div className={styles.skills__info_right}>
             <h3>{skills[activeIndex].title}</h3>
             <div>{skills[activeIndex].desc}</div>
-            <div className={styles.skills__level} data-level={skills[activeIndex].level}>{skills[activeIndex].level}</div>
+            <div className={styles.skills__level} data-level={skills[activeIndex].level}>{skills[activeIndex].level} level</div>
           </div>
         </div>
       </div>
